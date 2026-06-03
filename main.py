88b924f71acf2c2bbb24601ee61291a8b7fae01f
Pratik Bhuvad <pbhuvad55@gmail.com>
2026-06-03 20:59:05 +0530

    feat: connect ranker output to analyzer pipeline

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.Ingest_Layer.ingestion import ingest_data
 from src.Github_Layer.orchestrator import orchestrate_github_search_with_stats
+from src.Ranker.orchestrator import run_ranking_pipeline
+from src.content_Analyzer.orchestrator import run_analysis_pipeline
+from src.config import GITHUB_HEADER as headers
 
 def main():
     data = ingest_data()
@@ -7,7 +10,10 @@ def main():
         return
     repos = orchestrate_github_search_with_stats(data)
     
-    print("Extracted Repositories:" ,repos)
+    scored_repos = run_ranking_pipeline(repos.get("repositories", []))
+    
+    analyzed_repos = run_analysis_pipeline(scored_repos, headers)
+    
 
 if __name__ == "__main__":
     main()
