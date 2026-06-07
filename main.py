a7c88267a89e7ffe90a855900f685079623a27c3
Pratik Bhuvad <pbhuvad55@gmail.com>
2026-06-07 12:32:05 +0530

    feat: recommendation pipeline

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from src.Ranker.orchestrator import run_ranking_pipeline
 from src.Content_Layer.orchestrator import fetch_content_for_repos
 
 from src.Content_Layer.orchestrator import fetch_content_for_repos
+
+from src.Content_Layer.orchestrator import fetch_content_for_repos
 from src.Analyzer.orchestrator import analyze_repository_content
 
+from src.Recommend_Layer.orchestrator import run_recommend_ranking_pipeline
 
 from src.config import GITHUB_HEADER as headers
 
@@ -21,15 +24,17 @@ def main():
     repos = orchestrate_github_search_with_stats(data)
     
     scored_repos = run_ranking_pipeline(repos.get("repositories", []))
-    
 
     content_fetched = fetch_content_for_repos(scored_repos, headers)
     
     analyzed_repos = analyze_repository_content(content_fetched) 
     
-    for repo in analyzed_repos:
-        print(f"{repo['owner']}/{repo['title']}: {repo['content_score']:.2f}: Readme Score: {repo['readme_score']:.2f}, Tree Score: {repo['tree_score']:.2f} :: Score:{repo['score']} :: TOPICS: {', '.join(repo.get('topics', []))}", end="\n\n")   
+    final_recommendations_repos = run_recommend_ranking_pipeline(analyzed_repos)
     
+    for repo in final_recommendations_repos:
+        filtered_repo = {k: v for k, v in repo.items() if k not in ('readme_content', 'file_structure')}
+        print(filtered_repo, "\n\n","=" * 50 )
+
 
 if __name__ == "__main__":
     main()
